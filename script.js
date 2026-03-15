77f9750b0d493c815f68bc1103b7da45c0589114
artistgovindkrish-del <artistgovind.krish@gmail.com>
2026-03-15 17:44:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,14 +10,23 @@ document.querySelectorAll(".art").forEach(el=>{
   observer.observe(el);
 });
 
+<!--fetch header & footer html file-->
 fetch("header.html")
 .then(response => response.text())
 .then(data => {
 document.getElementById("header").innerHTML = data;
 });
-
 fetch("footer.html")
 .then(response => response.text())
 .then(data => {
 document.getElementById("footer").innerHTML = data;
 });
+
+<!--Open viewer to enlage images-->
+function openViewer(img){
+document.getElementById("viewer").style.display="flex";
+document.getElementById("viewer-img").src=img.src;
+}
+function closeViewer(){
+document.getElementById("viewer").style.display="none";
+}
